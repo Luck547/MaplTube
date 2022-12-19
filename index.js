bbc768fbc58f0040d6d074842b6afc4f7f7e3a26
Alan Lucciano Pindo <alan.lucciano@gmail.com>
2022-12-19 11:01:20 +0100

    In the middle of.

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+
+//Begin
+
+// Import the express and passport module
 const express = require('express');
 const passport = require('passport');
+
+// Bring back the passport config
 require('./auth');
 
+// Create an express app
 const app = express();
 
+// Create a route for the home page
 app.get('/', (req, res) => {
   res.send('<a href="/auth/google">Authenticate to YouTube</a>');
 });
 
+// Create a route for the authentication
 app.get('/auth/google',
     passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-app.get('/protected', (req, res) => {
+// Create a route for the callback
+app.get('/oauth2callback', passport.authenticate('google', {
+    successRedirect: '/getRating',
+    failureRedirect: '/auth/failure'
+}));
+
+// Create a route for the failure
+app.get('/auth/failure', (req, res) => {
+    res.send('Failed to authenticate');
+});
+
+// Create a route protected by the authentication
+app.get('/getRating', (req, res) => {
     res.send('This is protected');
 });
 
@@ -19,4 +40,9 @@ app.get('/protected', (req, res) => {
 app.listen(4200, () => {
     console.log('App listening on port 4200 :)');
 });
+
+
+
+// End
+
 
